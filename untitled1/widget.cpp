f2b4c005a7a5ee6ac19bdf17e53c7753569f807d
morgan5250 <2509071851@qq.com>
2023-08-17 16:58:24 +0800

    test8_17

---
+++ untitled1/widget.cpp
@@ -0,0 +1,29 @@
+﻿
+#include "widget.h"
+#include "ui_widget.h"
+
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    //仅限于当期前QLabel
+    ui->label->setStyleSheet("QLabel{color:rgb(0,255,255);"
+                             "background-color:rgb(255,150,234)"
+                             "background-image:url(:/new/prefix1/im/Frame.jpg)"
+                             "}");
+
+    //用与全部QLabel
+    this->setStyleSheet("QLabel{color:rgb(0,255,255);"
+                             "background-color:red"
+                             "}");
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
